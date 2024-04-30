5f3f1d4dad56b0d5fc59d146f01e74d5846d79de
Caner Ates <cnrates@gmail.com>
2024-04-30 15:10:23 +0300

    Initial Commit

---
+++ SwiftUI-UIKit-Interoperability/SwiftUI_UIKit_InteroperabilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UIKit_InteroperabilityApp.swift
+//  SwiftUI-UIKit-Interoperability
+//
+//  Created by Caner Ates on 30.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UIKit_InteroperabilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
